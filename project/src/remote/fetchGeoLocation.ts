2541306a9c65057c6c271eba86e0c9d34b6c0df5
codearis <arielzius@gmail.com>
2022-06-03 11:59:26 -0300

    changed http fetch url to https

--- project/src/remote/fetchGeoLocation.ts
+++ project/src/remote/fetchGeoLocation.ts
@@ -6,7 +6,7 @@ interface Coordinates {
 
 export const fetchGeolocation = async (city: string) => {
   const api = process.env.REACT_APP_OPEN_API;
-  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${api}`;
+  const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${api}`;
 
   if (city) {
     return fetch(url).then(async (response) => {
